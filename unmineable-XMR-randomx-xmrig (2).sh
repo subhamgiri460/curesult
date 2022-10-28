537350fda980c48c48afd2c385ed02f002409acf
subhamgiri460 <76942112+subhamgiri460@users.noreply.github.com>
2022-10-28 15:23:46 +0530

    Add files via upload

---
+++ unmineable-XMR-randomx-xmrig (2).sh
@@ -0,0 +1,1 @@
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:4444 -u XMR:42goDTyZng8izb9nTf7TYcW2TJhvQSvX2gw87VEAdTvTUxfaBxx395cZcDoDgwnm2qi4PbLjDfvJTNPE8ZUJLAT355TFhVv.unmineable_worker_hjrwrmbm -p x 
